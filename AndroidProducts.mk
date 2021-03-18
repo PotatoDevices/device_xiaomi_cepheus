c9d88f6912b2690bcc24d3bbd53e17a29bda3d23
Giovix92 <ggualtierone@gmail.com>
2021-03-18 01:36:46 +0000

    cepheus: Revenge-ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_cepheus.mk
+    $(LOCAL_DIR)/revengeos_cepheus.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_cepheus-user \
-    aosp_cepheus-userdebug \
-    aosp_cepheus-eng
+    revengeos_cepheus-user \
+    revengeos_cepheus-userdebug \
+    revengeos_cepheus-eng
--- revengeos_cepheus.mk
+++ revengeos_cepheus.mk
@@ -22,13 +22,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/cepheus/device.mk)
 
 # Inherit some common Revenge stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/revengeos/configs/common.mk)
 
 # Boot animation
 TARGET_BOOT_ANIMATION_RES := 1080
+REVENGEOS_BUILDTYPE := OFFICIAL
 
 # Device identifier
-PRODUCT_NAME := aosp_cepheus
+PRODUCT_NAME := revengeos_cepheus
 PRODUCT_DEVICE := cepheus
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi 9
