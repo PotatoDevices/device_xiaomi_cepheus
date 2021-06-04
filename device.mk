32886ac6ad230fcdaadf6d8e1656d2f7ba4a386b
Giovix92 <ggualtierone@gmail.com>
2021-06-04 12:14:55 +0200

    cepheus: drop entirely parts

--- device.mk
+++ device.mk
@@ -124,10 +124,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-sp/libc++.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libc++.so
 
-# Device-specific settings
-PRODUCT_PACKAGES += \
-    XiaomiParts
-
 # Display
 PRODUCT_PACKAGES += \
     disable_configstore
